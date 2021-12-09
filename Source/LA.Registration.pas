d5bfe10733e56d518907f38ee8f0211d7fa104ed
lagodny <a.lagodny@gmail.com>
2021-12-09 18:08:01 +0200

    регистрация компонентов

---
+++ Source/LA.Registration.pas
@@ -0,0 +1,20 @@
+﻿unit LA.Registration;
+
+interface
+
+procedure Register;
+
+implementation
+
+uses
+  System.Classes,
+  LA.Data.Sensor,
+  LA.Net.Connector.Http,
+  LA.Data.Updater;
+
+procedure Register;
+begin
+  RegisterComponents('LA', [TDCSensor, TDCHttpConnector, TDataUpdater]);
+end;
+
+end.
