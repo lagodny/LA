547563699096156c24b5425df25143374cb6510b
lagodny <a.lagodny@gmail.com>
2021-12-12 09:14:00 +0200

    Тест получения данных на Андроиде

---
+++ Test/_FMX/TestSensorListFMX/Test.SensorList.Main.pas
@@ -0,0 +1,41 @@
+﻿unit Test.SensorList.Main;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, LA.Data.Source, LA.Data.Updater, LA.Net.Connector, LA.Net.Connector.Http,
+  LA.Data.Sensor, Data.Bind.EngExt, Fmx.Bind.DBEngExt, System.Rtti, System.Bindings.Outputs, Data.Bind.Components, FMX.Objects,
+  Fmx.Bind.Editors, FMX.Controls.Presentation, FMX.StdCtrls;
+
+type
+  TForm3 = class(TForm)
+    LASensorList1: TLASensorList;
+    DCHttpConnector1: TDCHttpConnector;
+    DataUpdater1: TDataUpdater;
+    LASensor1: TLASensor;
+    LASensor2: TLASensor;
+    LASensor3: TLASensor;
+    Text1: TText;
+    BindingsList1: TBindingsList;
+    Text2: TText;
+    Text3: TText;
+    LinkControlToPropertyText3: TLinkControlToProperty;
+    CheckBox1: TCheckBox;
+    LinkControlToPropertyActive: TLinkControlToProperty;
+    LinkControlToPropertyText: TLinkControlToProperty;
+    LinkControlToPropertyText2: TLinkControlToProperty;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form3: TForm3;
+
+implementation
+
+{$R *.fmx}
+
+end.
---
+++ Test/_FMX/TestSensorListFMX/TestSensorListFMX.dpr
@@ -0,0 +1,14 @@
+﻿program TestSensorListFMX;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  Test.SensorList.Main in 'Test.SensorList.Main.pas' {Form3};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm3, Form3);
+  Application.Run;
+end.
