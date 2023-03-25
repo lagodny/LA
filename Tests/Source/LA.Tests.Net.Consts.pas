4c71a052f80e616c126dea79e859b0d3a46b9e18
Oleksandr Lagondnyi <a.lagodny@gmail.com>
2023-03-25 12:20:19 +0200

    define localhost / tdc

--- Tests/Source/LA.Tests.Net.Consts.pas
+++ Tests/Source/LA.Tests.Net.Consts.pas
@@ -2,11 +2,23 @@
 
 interface
 
+{.$DEFINE TDC}
+{$DEFINE localhost}
+
 const
   cUserName = 'demo';
   cPassword = 'demo';
+
+{$IFDEF TDC}
   cHttpAddr = 'dc.tdc.org.ua:80';//'http://dc.tdc.org.ua:80';
   cHttpsAddr = 'https://dc.tdc.org.ua:443';
+{$ENDIF}
+
+{$IFDEF localhost}
+  cHttpAddr = 'localhost:80';//'http://dc.tdc.org.ua:80';
+  cHttpsAddr = 'https://localhost:443';
+{$ENDIF}
+
 
 implementation
 
