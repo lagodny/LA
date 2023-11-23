af1a25936b25175e6b24e8c538e163273325e192
Oleksandr Lagondnyi <a.lagodny@gmail.com>
2023-11-23 21:44:10 +0200

    added style for LALabel

--- Source/FMX/LA.FMX.StdCtrls.pas
+++ Source/FMX/LA.FMX.StdCtrls.pas
@@ -3,18 +3,34 @@
 interface
 
 uses
-  System.Classes,
-  FMX.StdCtrls,
+  System.Classes, System.Types, System.UITypes, System.SysUtils,
+  FMX.Types, FMX.Objects, FMX.StdCtrls, FMX.Graphics, FMX.Styles, FMX.Layouts,
+  LA.Data.Types,
   LA.Data.Link.Sensor, LA.Data.Link.Sensor.Intf,
   LA.Data.Source;
 
 type
+  [ComponentPlatforms(pidAllPlatforms)]
   TLALabel = class(TLabel, ILASensorLink)
   private
     FLink: TLASensorLink;
+    FLastLinkStatus: string;
     procedure SetLink(const Value: TLASensorLink);
   protected
+    procedure ApplyStyle; override;
+
+    function GetDefaultSize: TSizeF; override;
+    function GetStyleObject: TFmxObject; override;
+
     procedure Notification(AComponent: TComponent; Operation: TOperation); override;
+    /// возвращает текстовое представление данных линка для выбора соответствующего стиля отображения (суфикс добавляется к стилю)
+    ///  'default' - данные в порядке
+    ///  'error' - ошибка
+    ///  'h' - превышение высокого уровня - предупреждение
+    ///  'hh' - превышение максимально высокого уровня - авария
+    ///  'l' - низкий уровень - предупреждение
+    ///  'll' - низкий уровень - авария
+    function GetLinkStatus: string;
   public
     constructor Create(AOwner: TComponent); override;
     destructor Destroy; override;
@@ -27,13 +43,57 @@ type
 
 implementation
 
+//{$R *.all.res}
+{$R LA.FMX.StdCtrls.all.RES}
+
+
+const
+  cLALabelStyleName = 'LALabelStyle';
+
+
 { TLALabel }
+
+procedure TLALabel.ApplyStyle;
+var
+  aLayout: TLayout;
+begin
+  inherited;
+
+  /// нужно сделать невидимыми все лайауты кроме того, который соответствует состоянию данных
+  if FindStyleResource<TLayout>('layout', aLayout) then
+  begin
+    for var c in aLayout.Controls do
+      if (c is TRectangle) then
+        c.Visible := SameText(c.StyleName, FLastLinkStatus);
+  end;
+end;
+//var
+//  r: TRectangle;
+//begin
+//  inherited;
+//
+//  if Link.Status <> '' then
+//  begin
+//    if FindStyleResource<TRectangle>(cSysleResBackground, r) then
+//    begin
+//      r.Stroke.Dash := TStrokeDash.Dash;
+//      r.Stroke.Color := TAlphaColorRec.Coral;
+//      r.Fill.Color := TAlphaColorRec.Lemonchiffon;
+//    end;
+//  end;
+//end;
 
 constructor TLALabel.Create(AOwner: TComponent);
 begin
   inherited;
   FLink := TLASensorLink.Create(Self);
   FLink.OnOwnerNotify := DoDataLinkChanged;
+
+  FLastLinkStatus := 'default';
+
+  TextSettings.HorzAlign := TTextAlign.Center;
+  Size.Width := 100;
+  Size.Height := 25;
 end;
 
 destructor TLALabel.Destroy;
@@ -49,6 +109,50 @@ begin
     Exit;
 
   Text := Link.Text;
+
+  if FLastLinkStatus <> GetLinkStatus then
+  begin
+    FLastLinkStatus := GetLinkStatus;
+    ApplyStyle;
+  end;
+end;
+
+function TLALabel.GetDefaultSize: TSizeF;
+begin
+  Result := TSizeF.Create(100, 25);
+end;
+
+function TLALabel.GetLinkStatus: string;
+begin
+  if (Link.Status <> '') or (Link.StatusCode <> 0) then
+    Result := 'error'
+  else
+  begin
+    case Link.ValueRange.Check(Link.Value) of
+      Correct, NoRange:
+        Result := 'default';
+      LowLow:
+        Result := 'LL';
+      Low:
+        Result := 'L';
+      High:
+        Result := 'H';
+      HighHigh:
+        Result := 'HH';
+      InTarget:
+        Result := 'T';
+      NoTarget:
+        Result := 'NT';
+    end;
+  end;
+end;
+
+function TLALabel.GetStyleObject: TFmxObject;
+begin
+  if StyleLookup = '' then
+    Result := TFmxObject(TStyleStreaming.LoadFromResource(HInstance, cLALabelStyleName, RT_RCDATA))
+  else
+    Result := inherited GetStyleObject;
 end;
 
 procedure TLALabel.Notification(AComponent: TComponent; Operation: TOperation);
