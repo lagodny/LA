932fd04077c0096df37ab6baa87e0c4bbf206e4a
Oleksandr Lagondnyi <a.lagodny@gmail.com>
2023-11-25 08:08:27 +0200

    init

---
+++ Source/LA.Data.Connection.Manager.pas
@@ -0,0 +1,28 @@
+﻿unit LA.Data.Connection.Manager;
+
+interface
+
+uses
+  System.Classes;
+
+type
+  TLAConnectionData = class(TPersistent)
+  private
+    FName: string;
+  published
+    property Name: string read FName write FName;
+  end;
+
+
+  /// предназначен для управления подлкючениями к серверам Мониторинга
+  ///  - для каждого подключения:
+  ///     - хранилище лукапов: поиск по имени таблицы
+  ///     - кеш иерархии
+  ///     - очередь задач
+  TLAConnectionManager = class(TComponent)
+
+  end;
+
+implementation
+
+end.
