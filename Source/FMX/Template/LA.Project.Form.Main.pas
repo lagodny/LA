92e59295b52375d646d829f7ea5f0455bbbf1472
Oleksandr Lagondnyi <a.lagodny@gmail.com>
2023-11-25 08:08:48 +0200

    project template

---
+++ Source/FMX/Template/LA.Project.Form.Main.pas
@@ -0,0 +1,24 @@
+﻿unit LA.Project.Form.Main;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+
+type
+  TMain = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Main: TMain;
+
+implementation
+
+{$R *.fmx}
+
+end.
---
+++ Source/FMX/Template/LAProjectTemplate.dpr
@@ -0,0 +1,15 @@
+﻿program LAProjectTemplate;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  LA.Project.Form.Main in 'LA.Project.Form.Main.pas' {Main},
+  LA.Project.DM.Data in 'LA.Project.DM.Data.pas' {DMData: TDataModule};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TMain, Main);
+  Application.Run;
+end.
