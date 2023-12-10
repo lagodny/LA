72f93f04dc7e0e404212e52ff4366e981b0ec283
Oleksandr Lagondnyi <a.lagodny@gmail.com>
2023-12-10 09:13:51 +0200

    TLASensorUpdaterEditor, TLAConnectionManagerEditor

--- Source/LA.Designer.Regisration.pas
+++ Source/LA.Designer.Regisration.pas
@@ -14,19 +14,41 @@ type
     function GetVerbCount: Integer; override;
   end;
 
+  TLASensorUpdaterEditor = class(TComponentEditor)
+  public
+    procedure ExecuteVerb(Index: Integer); override;
+    function GetVerb(Index: Integer): string; override;
+    function GetVerbCount: Integer; override;
+  end;
+
+  TLAConnectionManagerEditor = class(TComponentEditor)
+  public
+    procedure ExecuteVerb(Index: Integer); override;
+    function GetVerb(Index: Integer): string; override;
+    function GetVerbCount: Integer; override;
+  end;
+
+
 procedure Register;
 
 implementation
 
 uses
-  LA.Data.Sensor;
+  LA.Data.Sensor,
+  LA.Data.Sensor.Updater,
+  LA.Data.Connection.Manager;
 
 resourcestring
   sDesAddSensor = 'Add sensor';
+  sInitSensors = 'Init sensors from server';
+  sConnectLinksToHistoryViewer = 'Connect links to HistoryViewer';
+  sDisconnectLinksFromHistoryViewer = 'Disconnect links from HistoryViewer';
 
 procedure Register;
 begin
   RegisterComponentEditor(TLASensorList, TLASensorListEditor);
+  RegisterComponentEditor(TLASensorUpdater, TLASensorUpdaterEditor);
+  RegisterComponentEditor(TLAConnectionManager, TLAConnectionManagerEditor);
 end;
 
 
@@ -54,6 +76,71 @@ end;
 function TLASensorListEditor.GetVerbCount: Integer;
 begin
   Result := 1;
+end;
+
+{ TLASensorUpdaterEditor }
+
+procedure TLASensorUpdaterEditor.ExecuteVerb(Index: Integer);
+begin
+  case Index of
+    0:
+    begin
+      if Component is TLASensorUpdater then
+        TLASensorUpdater(Component).InitItems;
+    end;
+  end;
+end;
+
+function TLASensorUpdaterEditor.GetVerb(Index: Integer): string;
+begin
+  case Index of
+    0: Result := sInitSensors;
+  end;
+end;
+
+function TLASensorUpdaterEditor.GetVerbCount: Integer;
+begin
+  Result := 1;
+end;
+
+{ TLAConnectionManagerEditor }
+
+procedure TLAConnectionManagerEditor.ExecuteVerb(Index: Integer);
+var
+  i: Integer;
+  aConnection: TLAConnectionItem;
+begin
+  case Index of
+    0:
+    begin
+      if Component is TLAConnectionManager then
+      begin
+        for i := 0 to TLAConnectionManager(Component).Items.Count - 1 do
+        begin
+          aConnection := TLAConnectionManager(Component).Items[i];
+          TLAConnectionManager(Component).HistoryViewer.ConnectLinksFromDataSource(aConnection.Updater);
+        end;
+      end;
+    end;
+    1:
+    begin
+      if Component is TLAConnectionManager then
+        TLAConnectionManager(Component).HistoryViewer.DisconnectLinks;
+    end;
+  end;
+end;
+
+function TLAConnectionManagerEditor.GetVerb(Index: Integer): string;
+begin
+  case Index of
+    0: Result := sConnectLinksToHistoryViewer;
+    1: Result := sDisconnectLinksFromHistoryViewer;
+  end;
+end;
+
+function TLAConnectionManagerEditor.GetVerbCount: Integer;
+begin
+  Result := 2;
 end;
 
 end.
